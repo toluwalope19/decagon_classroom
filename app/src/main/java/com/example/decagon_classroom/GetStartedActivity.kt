614883428976ccb2fd790b25161016bf025cb528
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-11-20 17:59:30 +0100

    welcome screen

--- app/src/main/java/com/example/decagon_classroom/GetStartedActivity.kt
+++ app/src/main/java/com/example/decagon_classroom/GetStartedActivity.kt
@@ -3,7 +3,7 @@ package com.example.decagon_classroom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GetStartedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
