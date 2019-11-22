a81cd107d6a7b4160e41fa9c2e3976f7abf6676c
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-11-22 16:03:01 +0100

    nav-bar added

--- app/src/main/java/com/example/decagon_classroom/GetStartedActivity.kt
+++ app/src/main/java/com/example/decagon_classroom/GetStartedActivity.kt
@@ -1,12 +1,22 @@
 package com.example.decagon_classroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_get_started.*
 
 class GetStartedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
+
+        sign_up.setOnClickListener {
+
+            val intent  = Intent(this, RegisterActivity::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/decagon_classroom/RegisterActivity.kt
+++ app/src/main/java/com/example/decagon_classroom/RegisterActivity.kt
@@ -1,18 +1,23 @@
 package com.example.decagon_classroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_get_started.*
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_verification.*
+import kotlinx.android.synthetic.main.app_bar.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
 
         Email.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s : Editable?){}
@@ -26,11 +31,18 @@ class RegisterActivity : AppCompatActivity() {
             }
         })
 
+
+
+        back_button.setOnClickListener {
+            finish()
+        }
+
+
         //form validation
         Email.setText("")
         val emailPattern = ("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+").toRegex()
 
-        button.setOnClickListener {
+        submit_register.setOnClickListener {
             var email = Email.text.toString()
 
             if(email.isEmpty()){
