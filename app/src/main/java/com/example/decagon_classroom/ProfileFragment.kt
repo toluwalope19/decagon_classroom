c33ec68e1d3731cfd57ec88c635ddd9d7eb62de4
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-11-26 10:34:40 +0100

    profile screen

--- app/src/main/java/com/example/decagon_classroom/ProfileFragment.kt
+++ app/src/main/java/com/example/decagon_classroom/ProfileFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.search_bar.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,7 +20,7 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
 
-
+    search_button.visibility = View.GONE
     }
 
 
