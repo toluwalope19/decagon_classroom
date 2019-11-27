548280c1a1edc183fa55543be72225a2901a797b
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-11-27 10:02:51 +0100

    tabs switching

--- app/src/main/java/com/example/decagon_classroom/ResourcesFragment.kt
+++ app/src/main/java/com/example/decagon_classroom/ResourcesFragment.kt
@@ -6,19 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_resources.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ResourcesFragment : Fragment() {
 
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resources, container, false)
+        val view = inflater.inflate(R.layout.fragment_resources, container, false)
+
+        val viewPager = view.findViewById<ViewPager>(R.id.viewPager)
+        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
+
+        viewPager.adapter= TabAdapter(tabLayout.tabCount, fragmentManager!!)
+
+
+        return view
     }
+
 
 
 }
