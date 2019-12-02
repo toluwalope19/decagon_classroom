b42aaafdceeea8563bf7ccef893112ece5b4cef2
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2019-12-02 16:41:54 +0100

    viewpager 2

--- app/src/main/java/com/example/decagon_classroom/HomeFragment.kt
+++ app/src/main/java/com/example/decagon_classroom/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.decagon_classroom
 
 
+import android.gesture.GestureOverlayView.ORIENTATION_HORIZONTAL
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
@@ -34,6 +36,7 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_homepage, container, false)
 
+        val viewpager2 = view.findViewById<ViewPager2>(R.id.viewPager2)
         var books = arrayListOf<Books>(
 
             Books("Anjana","Meeee"),
@@ -41,7 +44,28 @@ class HomeFragment : Fragment() {
             Books("Anjana","Meeee"),
             Books("Anjana","Meeee")
         )
-        val viewpager2 = view.findViewById<ViewPager2>(R.id.viewPager2)
+        with(viewpager2) {
+            clipToPadding = false
+            clipChildren = false
+            offscreenPageLimit = 3
+        }
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin)
+        val offsetPx = resources.getDimensionPixelOffset(R.dimen.offset)
+        viewpager2.setPageTransformer { page, position ->
+            val viewPager = page.parent.parent as ViewPager2
+            val offset = position * -(2 * offsetPx + pageMarginPx)
+            if (viewPager.orientation == ORIENTATION_HORIZONTAL) {
+                if (ViewCompat.getLayoutDirection(viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                    page.translationX = -offset
+                } else {
+                    page.translationX = offset
+                }
+            } else {
+                page.translationY = offset
+            }
+        }
+
+
         viewpager2.adapter = CardAdapter(books)
 
 
